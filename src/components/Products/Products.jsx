567b387a129cc52c38ca3aee0d69543c5790d0e0
Parada <paradafaii28@gmail.com>
2021-08-24 14:43:31 -0400

    connect with Acommerce api

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
-const products =[
-    {id:1, name: "Headphones", description: 'Great headphones', price: '$150', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsZihVcyLo8OeN2qlCcYT2a25JnenslxWx4A&usqp=CAU'},
-    {id:2, name: "Shoes", description: 'Running shoes', price: '$75', image: 'https://i.pcmag.com/imagery/reviews/046UyKcU86Megq3Eenq0sbo-1.1597063431.fit_lpad.size_625x365.jpg'},
-]
 
-const Products = () => {
-    return (
-        <main>
+const Products = ({ products }) => {
+    const classes = useStyles();
+        return (
+        <main className={classes.content}>
+            <div className={classes.toolbar}></div>
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} sm={6} md={4} lg={3}>
