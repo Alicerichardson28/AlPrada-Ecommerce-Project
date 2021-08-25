74aba5de9fa5d3b6dc03ad63c80bcaf7effafb98
Parada <paradafaii28@gmail.com>
2021-08-25 16:48:44 -0400

    using react router dom for navigatior between pages

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Container, Typography, Button, Grid, MenuItem, ListItemIcon } from '@material-ui/core'
-import { classes } from 'istanbul-lib-coverage'
-
+import { Container, Typography, Button, Grid } from '@material-ui/core'
 import useStyles from './styles'
+
+import CartItem from './CartItem/CartItem'
 
 const Cart = ({ cart }) => {
     const classes = useStyles();
@@ -14,11 +14,11 @@ const Cart = ({ cart }) => {
     const FilledCart = () => (
         <>
             <Grid container spacing={3}>
-                {cart.line_items.map((item) => {
+                {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <div>{item.name}</div>
+                        <CartItem item={item}/>
                     </Grid>
-                })}
+                ))}
             </Grid>
             <div className={classes.cardDetails}>
                 <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
@@ -31,10 +31,11 @@ const Cart = ({ cart }) => {
     )
 
     if(!cart.line_items) return 'Loading....'
+
     return (
         <Container>
             <div className={classes.toolbar}/>
-            <Typography className={classes.title} variant="h3">Your Shopping Cart</Typography>
+            <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
             { !cart.line_items.length ? <EmptyCart /> : <FilledCart />}
         </Container>
     )
